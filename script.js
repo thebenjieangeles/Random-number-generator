003ddf07e1224cab5790a5768760ce66c2c4c6f6
thebenjieangeles <thebenjangeles@gmail.com>
2025-06-17 14:23:34 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var askName = prompt("What is your name?");
+var askName2 = prompt("What is your crush's name?");
+var n = Math.random();
+var result = Math.floor(n * 100) + 1;
+alert(askName + " and " + askName2 + "'s compatibility is " + result + "%");
