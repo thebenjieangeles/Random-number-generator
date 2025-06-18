f50a8af74897444793a372cb951219de4a2c5fd9
thebenjieangeles <thebenjangeles@gmail.com>
2025-06-18 13:38:58 +0800

    added condiional if - else

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ var askName = prompt("What is your name?");
 var askName2 = prompt("What is your crush's name?");
 var n = Math.random();
 var result = Math.floor(n * 100) + 1;
-alert(askName + " and " + askName2 + "'s compatibility is " + result + "%");
+if (result > 70) {
+  alert(
+    askName +
+      " and " +
+      askName2 +
+      "'s compatibility is " +
+      result +
+      "% you are compatible"
+  );
+} else {
+  alert(askName + " and " + askName2 + "'s compatibility is " + result + "%");
+}
