240ada470ff0d79693431be10fd5055411d485bc
thebenjieangeles <thebenjangeles@gmail.com>
2025-06-19 13:36:15 +0800

    added more if statements with comparators

--- script.js
+++ script.js
@@ -11,6 +11,21 @@ if (result > 70) {
       result +
       "% you are compatible"
   );
+}
+
+if (result > 30 && result <= 70) {
+  alert(askName + " and " + askName2 + "'s compatibility is " + result + "%");
+}
+
+if (result <= 30) {
+  alert(
+    askName +
+      " and " +
+      askName2 +
+      "'s compatibility is " +
+      result +
+      "% di kayo bagay"
+  );
 } else {
   alert(askName + " and " + askName2 + "'s compatibility is " + result + "%");
 }
